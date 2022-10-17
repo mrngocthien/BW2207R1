c88d78c9e158d606da30079da236edbbee16219c
mrngocthien <108567256+mrngocthien@users.noreply.github.com>
2022-10-18 05:26:46 +0700

    up

--- Effect-bundle/video-popup/script.js
+++ Effect-bundle/video-popup/script.js
@@ -1,12 +1,23 @@
-let btn = document.querySelector('.btn');
-let clip = document.querySelector('.clip');
-let close = document.querySelector('.close');
-btn.onclick = function() {
-    btn.classList.add('active');
-    clip.classList.add('active');
-}
-close.onclick = function() {
-    btn.classList.remove('active');
-    clip.classList.remove('active');
-    
-}
+window.document.onkeydown = function(e) {
+    if (!e) {
+      e = event;
+    }
+    if (e.keyCode == 27) {
+      lightbox_close();
+    }
+  }
+  
+  function lightbox_open() {
+    var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+    window.scrollTo(0, 0);
+    document.getElementById('light').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+    lightBoxVideo.play();
+  }
+  
+  function lightbox_close() {
+    var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+    document.getElementById('light').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+    lightBoxVideo.pause();
+  }
